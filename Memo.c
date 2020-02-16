1141a90e30333c23171fd1a51b4e0d107b57e43e
bicci34@gmail.com <bicci34@gmail.com>
2020-02-16 23:02:33 +0100

    cleanup code

--- Memo.c
+++ Memo.c
@@ -14,7 +14,7 @@ MODULE_VERSION("1.0");
 /*Global variables*/
 #define DEVICE_NAME "Memo"
 #define SUCCESS 0
-static int memo_major = 0;
+static int memo_major = 0; 
 static int device_Open = 0;	/* Is device open? */
 
 static char* msg = NULL ;
@@ -36,8 +36,7 @@ static ssize_t memo_read(struct file *filp, char __user *buf, size_t count, loff
 /*Store user message*/
 static ssize_t memo_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos)
 {   
-	if(count != msg_size) {//check if the new message has a different length than the previous one
-		printk(KERN_DEBUG "Need msg reallocation to char =%ld\n",count);
+	if(count != msg_size) {//check if the new message has a different length than the previous one		
 		if (msg){// clear message when the message contains value 			
 			kfree(msg);
 		}
@@ -52,8 +51,7 @@ static ssize_t memo_write(struct file *filp, const char __user *buf, size_t coun
 }
 
 static int memo_open(struct inode *inode, struct file *filp)
-{
-	printk(KERN_DEBUG "Open method called\n");
+{	
 	if (device_Open){ //when file is already open return busy
 		return -EBUSY;
 	}
@@ -78,8 +76,7 @@ static struct file_operations memo_fops = { //Attach module operation
 
 /*Initialize new device*/
 int memo_init(void){
-	int result;
-	printk(KERN_DEBUG "\nInitialise memo\n");
+	int result;	
 	
 	result = register_chrdev(memo_major, DEVICE_NAME, & memo_fops);/* Assign major number, name and opration supported by this device */
 
@@ -89,7 +86,7 @@ int memo_init(void){
 	if (memo_major == 0)  {  
 
 		memo_major = result; /* Dynamic major number will be assignment */
-		printk(KERN_DEBUG "Major N = %d\n",memo_major);
+		printk(KERN_DEBUG "Memo Major Number = %d\n",memo_major);
 	} 
 	return SUCCESS;/*registration sucess and */
 }
